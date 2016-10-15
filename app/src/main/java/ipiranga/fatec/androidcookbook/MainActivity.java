c4824b585bc96b41acb7bf45cf1327edb7bd51f9
Eliete da Silva Rodrigues <eliete.rodrigues@concrete.com.br>
2016-10-15 10:31:27 -0300

    update gitignore e added toolbar

--- app/src/main/java/ipiranga/fatec/androidcookbook/MainActivity.java
+++ app/src/main/java/ipiranga/fatec/androidcookbook/MainActivity.java
@@ -2,12 +2,22 @@ package ipiranga.fatec.androidcookbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
